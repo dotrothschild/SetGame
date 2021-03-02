71818aa0850ef2cb2714238c9464d8e4bd32d542
Shimon Rothschild <shimon@inzhood.com>
2021-03-02 16:21:36 -0600

    Initial Commit

---
+++ SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by Shimon Rothschild on 2-03-21.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
